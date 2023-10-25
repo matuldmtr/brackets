3699e4209f0a55322a26d8de481d4855667f3b70
Dmitry Matul <matuldmtr@gmail.com>
2023-10-25 21:02:41 +0200

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let bracketsConfigMap = new Map(bracketsConfig);
+  let stack = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === bracketsConfigMap.get(stack[stack.length - 1])) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+  return stack.length === 0;
+};
